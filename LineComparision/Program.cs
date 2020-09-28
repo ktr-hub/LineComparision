b55e531334c15ff823974435db295c9b9398469b
KUMMARI TIRUPATHI RAO <35338095+ktr-hub@users.noreply.github.com>
2020-09-28 06:13:06 +0530

    [Tirupathi] Add. UC2 push

--- LineComparision/Program.cs
+++ LineComparision/Program.cs
@@ -24,9 +24,39 @@ namespace LineComparision
                     y[i / 2] = Convert.ToInt32(input[i]);
                 }
             }
-            Console.WriteLine("Length of the line is : ");
+            Console.WriteLine("Length of the first line is : ");
             int length = (int)Math.Sqrt((x[1] - x[0]) * (x[1] - x[0]) + (y[1] - y[0]) * (y[1] - y[0]));
             Console.WriteLine(length);
+
+            string[] input2 = new string[4];
+            int[] xx = new int[2];
+            int[] yy = new int[2];
+            Console.WriteLine("Enter xx1,yy1,xx2 and yy2");
+            for (int i = 0; i < 4; i++)
+            {
+                input2[i] = Console.ReadLine();
+                if (i % 2 == 0)
+                {
+                    xx[i / 2] = Convert.ToInt32(input2[i]);
+                }
+                else
+                {
+                    yy[i / 2] = Convert.ToInt32(input2[i]);
+                }
+            }
+            Console.WriteLine("Length of the second line is : ");
+            int length2 = (int)Math.Sqrt((xx[1] - xx[0]) * (xx[1] - xx[0]) + (yy[1] - yy[0]) * (yy[1] - yy[0]));
+            Console.WriteLine(length2);
+
+            if (length.Equals(length2))
+            {
+                Console.WriteLine("Both lines are equal");
+            }
+            else
+            {
+                Console.WriteLine("Both lines are not equal");
+            }
+
         }
     }
 }
