b5e02041c39b90cc7dfd790eef94afae6ab5b28c
KUMMARI TIRUPATHI RAO <35338095+ktr-hub@users.noreply.github.com>
2020-09-30 14:36:52 +0530

    [Tirupathi] Add. OOPs concepts UC4

---
+++ LineComparision/Line.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LineComparision
+{
+    class Line
+    {
+        int[] x = new int[2];
+        int[] y = new int[2];
+        Double length;
+
+        public void userInput()
+        {
+            Console.WriteLine("Enter end point coordinates as x1, y1, x2 and y2");
+            for (int i = 0; i < 4; i++)
+            {
+                string[] input = new string[4];
+                input[i] = Console.ReadLine();
+                if (i % 2 == 0)
+                {
+                    x[i / 2] = Convert.ToInt32(input[i]);
+                }
+                else
+                {
+                    y[i / 2] = Convert.ToInt32(input[i]);
+                }
+            }
+        }
+        public Double getLength()
+        {
+            length = Math.Sqrt((x[1] - x[0]) * (x[1] - x[0]) + (y[1] - y[0]) * (y[1] - y[0]));
+            return length;
+        }
+
+        public void compareToFunction(Double length1,Double length2)
+        {
+            if (length1.CompareTo(length2) == 0)
+            {
+                Console.WriteLine("Both lines are equal");
+            }
+            else if (length1.CompareTo(length2) > 0)
+            {
+                Console.WriteLine("Line 1 is greater than Line 2");
+            }
+            else
+            {
+                Console.WriteLine("Line 2 is greater than Line 1");
+            }
+        }
+
+    }
+}
--- LineComparision/Program.cs
+++ LineComparision/Program.cs
@@ -8,59 +8,17 @@ namespace LineComparision
     {
         static void Main(string[] args)
         {
-            string []input = new string[4];
-            int[] x = new int[2];
-            int[] y = new int[2];
-            Console.WriteLine("Enter x1,y1,x2 and y2");
-            for(int i = 0; i < 4; i++)
-            {
-                input[i] = Console.ReadLine();
-                if (i % 2 == 0)
-                {
-                    x[i / 2] = Convert.ToInt32(input[i]);
-                }
-                else
-                {
-                    y[i / 2] = Convert.ToInt32(input[i]);
-                }
-            }
-            Console.WriteLine("Length of the first line is : ");
-            int length = (int)Math.Sqrt((x[1] - x[0]) * (x[1] - x[0]) + (y[1] - y[0]) * (y[1] - y[0]));
-            Console.WriteLine(length);
+            Line line1 = new Line();
+            line1.userInput();
+            Double length1 = line1.getLength();
+            Console.WriteLine("Length of line 1 is : "+length1);
 
-            string[] input2 = new string[4];
-            int[] xx = new int[2];
-            int[] yy = new int[2];
-            Console.WriteLine("Enter xx1,yy1,xx2 and yy2");
-            for (int i = 0; i < 4; i++)
-            {
-                input2[i] = Console.ReadLine();
-                if (i % 2 == 0)
-                {
-                    xx[i / 2] = Convert.ToInt32(input2[i]);
-                }
-                else
-                {
-                    yy[i / 2] = Convert.ToInt32(input2[i]);
-                }
-            }
-            Console.WriteLine("Length of the second line is : ");
-            int length2 = (int)Math.Sqrt((xx[1] - xx[0]) * (xx[1] - xx[0]) + (yy[1] - yy[0]) * (yy[1] - yy[0]));
-            Console.WriteLine(length2);
+            Line line2 = new Line();
+            line2.userInput();
+            Double length2 = line2.getLength();
+            Console.WriteLine("Length of line 2 is : " + length2);
 
-            if (length.CompareTo(length2)==0)
-            {
-                Console.WriteLine("Both lines are equal");
-            }
-            else if(length.CompareTo(length2)>0)
-            {
-                Console.WriteLine("Line 1 is greater than Line 2");
-            }
-            else
-            {
-                Console.WriteLine("Line 2 is greater than Line 1");
-            }
-
+            line1.compareToFunction(length1, length2);
         }
     }
 }
